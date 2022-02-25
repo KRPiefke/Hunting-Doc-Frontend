80cb0d8c3afa5a255de52303495a750fe36ac6f5
Mattes Witthöft <matteswitti@gmail.com>
2022-02-25 14:30:56 +0100

    add appbar

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button, Container } from '@mui/material';
+import { Box } from '@mui/system';
+
+const App = () => {
+  return (
+    <AppBar position="sticky">
+      <Container maxWidth="lg">
+        <Toolbar>
+          <Box sx={{ display: { xs: 'none', sm: 'flex' }, alignItems: 'center', flexGrow: 1 }}>
+            <Typography variant="h5" sx={{ pl: 1 }}>
+              Hunting Doc
+            </Typography>
+          </Box>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default App;
