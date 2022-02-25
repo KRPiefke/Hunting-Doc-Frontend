7021c7cf2fce3f2e758af71d8bde653b2036b87f
Mattes Witthöft <matteswitti@gmail.com>
2022-02-25 21:14:12 +0100

    implement cssbaseline

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
@@ -15,7 +16,8 @@ const store = createStore(reducers, compose(applyMiddleware(thunk)));
 ReactDOM.render(
   <Provider store={store}>
     <ThemeProvider theme={theme}>
-    <App />
+      <CssBaseline enableColorScheme />
+      <App />
     </ThemeProvider>
   </Provider>,
   document.getElementById('root')
