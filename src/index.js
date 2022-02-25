61dff4ac8fcd99285c45a19d909c55ffb1ef70af
Mattes Witthöft <matteswitti@gmail.com>
2022-02-25 21:13:38 +0100

    implement a basic theme with provider

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from '@mui/material';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
+import theme from './utils/theme';
 import reducers from './reducers';
 
 import App from './App';
@@ -12,7 +14,9 @@ const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
+    <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
   </Provider>,
   document.getElementById('root')
 );
---
+++ src/utils/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1E5128',
+    },
+    secondary: {
+      main: '#FCB62A',
+    },
+  },
+});
+
+export default theme;
