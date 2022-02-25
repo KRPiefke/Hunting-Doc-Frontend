6d6ba5e818274a068797941581e7e7104f125c7b
Mattes Witthöft <matteswitti@gmail.com>
2022-02-25 16:02:10 +0100

    remove box arround the title

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Button, Container } from '@mui/material';
-import { styled, Box } from '@mui/system';
 
 const App = () => {
   return (
     <AppBar position="sticky">
       <Container maxWidth="lg">
         <Toolbar>
-          <Box sx={{ display: { xs: 'none', sm: 'flex' }, alignItems: 'center', flexGrow: 1 }}>
-            <Typography variant="h5">Hunting Doc</Typography>
-          </Box>
+          <Typography variant="h5" sx={{ display: { xs: 'none', sm: 'flex' }, alignItems: 'center', flexGrow: 1 }}>
+            Hunting Doc
+          </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
       </Container>
