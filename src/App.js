31f133d5f920de21c5f2e1d071d1ac4d3352501c
Mattes Witthöft <matteswitti@gmail.com>
2022-02-25 21:41:58 +0100

    positioning for login button in xs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Button, Container } from '@mui/material';
+import { Box } from '@mui/system';
 
 const App = () => {
   return (
     <AppBar position="sticky">
       <Container maxWidth="lg">
         <Toolbar>
-          <Typography variant="h5" sx={{ display: { xs: 'none', sm: 'flex' }, flexGrow: 1 }}>
-            Hunting Doc
-          </Typography>
+          <Box sx={{ flexGrow: 1 }}>
+            <Typography variant="h5" sx={{ display: { xs: 'none', sm: 'flex' } }}>
+              Hunting Doc
+            </Typography>
+          </Box>
           <Button color="inherit">Login</Button>
         </Toolbar>
       </Container>
