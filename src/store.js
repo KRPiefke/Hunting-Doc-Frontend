fd3450585a4ae7e0dbd5bc0973687b68a152a4b4
Mattes Witthöft <matteswitti@gmail.com>
2022-02-26 22:43:04 +0100

    configure redux-toolkit store

---
+++ src/store.js
@@ -0,0 +1,22 @@
+import allReducers from './reducers';
+import axiosInstance from './api/instance';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { handleRequests } from '@redux-requests/core';
+import { createDriver } from '@redux-requests/axios';
+
+const { requestsReducer, requestsMiddleware } = handleRequests({
+  driver: createDriver(axiosInstance),
+});
+
+const reducers = combineReducers({
+  requests: requestsReducer,
+  ...allReducers,
+});
+
+const store = configureStore({
+  reducer: reducers,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({ serializableCheck: false, immutableCheck: false }).concat(requestsMiddleware),
+});
+
+export default store;
