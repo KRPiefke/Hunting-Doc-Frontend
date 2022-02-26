e343714348eabbb208c5a531160374488a2b3d24
Mattes Witthöft <matteswitti@gmail.com>
2022-02-26 22:43:25 +0100

    import redux-toolkit store for provider

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
 
 import MyThemeProvider from './theme';
-import reducers from './reducers';
+import store from './store';
 
 import App from './App';
-
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
