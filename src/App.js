c1dd75b9977fde21e2cbb4b83e897719aeb713ea
Mattes Witthöft <matteswitti@gmail.com>
2022-02-26 20:41:49 +0100

    implement login dialog in app

--- src/App.js
+++ src/App.js
@@ -2,24 +2,29 @@ import React, { useState } from 'react';
 import { AppBar, Toolbar, Typography, Button, Container } from '@mui/material';
 import { Box } from '@mui/system';
 
+import LoginForm from './components/Auth/LoginForm';
+
 const App = () => {
   const [loginDialogOpen, setLoginDialogOpen] = useState(false);
 
   return (
-    <AppBar position="sticky">
-      <Container maxWidth="lg">
-        <Toolbar>
-          <Box sx={{ flexGrow: 1 }}>
-            <Typography variant="h5" sx={{ display: { xs: 'none', sm: 'flex' } }}>
-              Hunting Doc
-            </Typography>
-          </Box>
+    <div>
+      <AppBar position="sticky">
+        <Container maxWidth="lg">
+          <Toolbar>
+            <Box sx={{ flexGrow: 1 }}>
+              <Typography variant="h5" sx={{ display: { xs: 'none', sm: 'flex' } }}>
+                Hunting Doc
+              </Typography>
+            </Box>
             <Button color="inherit" onClick={() => setLoginDialogOpen(true)}>
               Login
             </Button>
-        </Toolbar>
-      </Container>
-    </AppBar>
+          </Toolbar>
+          <LoginForm open={loginDialogOpen} handleClose={() => setLoginDialogOpen(false)} />
+        </Container>
+      </AppBar>
+    </div>
   );
 };
 
