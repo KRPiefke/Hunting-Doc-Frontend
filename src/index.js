9aa0bd6b382002db7f22332a6fad7405b9d305fc
Mattes Witthöft <matteswitti@gmail.com>
2022-02-26 20:47:24 +0100

    change themeprovider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { ThemeProvider } from '@mui/material';
-import CssBaseline from '@mui/material/CssBaseline';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
-import theme from './utils/theme';
+import MyThemeProvider from './theme';
 import reducers from './reducers';
 
 import App from './App';
@@ -15,10 +13,9 @@ const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      <CssBaseline enableColorScheme />
+    <MyThemeProvider>
       <App />
-    </ThemeProvider>
+    </MyThemeProvider>
   </Provider>,
   document.getElementById('root')
 );
