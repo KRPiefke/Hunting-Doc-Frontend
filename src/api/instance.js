100b857130a04493b63e7f037922553246345813
Mattes Witthöft <matteswitti@gmail.com>
2022-02-27 19:15:45 +0100

    add content type json to axios instance

--- src/api/instance.js
+++ src/api/instance.js
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 const apiInstance = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
     withCredentials: true,
 });
 
