6126515f043a0173411a6334aab30d4ebc26569d
Mattes Witthöft <matteswitti@gmail.com>
2022-02-27 19:54:03 +0100

    implement token service in auth reducer for login

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,20 +1,18 @@
 import { createReducer } from '@reduxjs/toolkit';
 import { success, error } from '@redux-requests/core';
-import axiosInstance from '../api/instance';
+import tokenService from '../services/tokenService';
 import { login } from '../actions/auth';
 
 export default createReducer(
     {
-        accessToken: null,
         isLoggedIn: false,
         error: null,
     },
     {
         [success(login)]: (state, action) => {
-            let accessToken = action.payload.data.access_token;
-            state.accessToken = accessToken;
+            let accessToken = action.payload.data.accessToken;
             state.isLoggedIn = true;
-            axiosInstance.defaults.headers.authorization = 'Bearer ' + accessToken;
+            tokenService.setAccessToken(accessToken);
         },
         [error(login)]: (state, action) => {
             console.log(action.payload.response.data);
