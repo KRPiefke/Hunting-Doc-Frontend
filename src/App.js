55fe0e9068a6c80269364ebe194a18de65010984
Mattes Witthöft <matteswitti@gmail.com>
2022-02-28 00:19:52 +0100

    add menu to avatar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchAllShootings } from './actions/shootings';
 import { refreshToken } from './actions/auth';
-import { AppBar, Toolbar, Typography, Button, Container, Avatar } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button, Container, Avatar, Menu, MenuItem } from '@mui/material';
 import { Box } from '@mui/system';
 
 import LoginForm from './components/Auth/LoginForm';
@@ -10,6 +10,8 @@ import LoginForm from './components/Auth/LoginForm';
 const App = () => {
     const dispatch = useDispatch();
     const [loginDialogOpen, setLoginDialogOpen] = useState(false);
+    const [avatarMenuAnchorEl, setAvatarMenuAnchorEl] = useState(null);
+    const avatarMenuOpen = Boolean(avatarMenuAnchorEl);
     const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
 
     useEffect(() => {
@@ -23,6 +25,13 @@ const App = () => {
         }
     }, [isLoggedIn, dispatch]);
 
+    const handleAvatarMenuClick = event => {
+        setAvatarMenuAnchorEl(event.currentTarget);
+    };
+    const handleAvatarMenuClose = () => {
+        setAvatarMenuAnchorEl(null);
+    };
+
     return (
         <>
             <AppBar position="sticky">
@@ -34,12 +43,28 @@ const App = () => {
                             </Typography>
                         </Box>
                         {isLoggedIn ? (
-                            <Avatar sx={{ bgcolor: 'secondary.main' }}>AA</Avatar>
+                            <Avatar
+                                aria-controls={avatarMenuOpen ? 'avatar-menu' : undefined}
+                                aria-haspopup="true"
+                                aria-expanded={avatarMenuOpen ? 'true' : undefined}
+                                onClick={handleAvatarMenuClick}
+                                sx={{ bgcolor: 'secondary.main' }}>
+                                AA
+                            </Avatar>
                         ) : (
                             <Button color="inherit" onClick={() => setLoginDialogOpen(true)}>
                                 Login
                             </Button>
                         )}
+                        <Menu
+                            id="avatar-menu"
+                            anchorEl={avatarMenuAnchorEl}
+                            open={avatarMenuOpen}
+                            onClose={handleAvatarMenuClose}>
+                            <MenuItem autoFocus={false} onClick={handleAvatarMenuClose}>
+                                Abmelden
+                            </MenuItem>
+                        </Menu>
                     </Toolbar>
                     <LoginForm open={loginDialogOpen} handleClose={() => setLoginDialogOpen(false)} />
                 </Container>
