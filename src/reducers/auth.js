0b7dd30028c326858ea324c8d3324f95702ce6c8
Mattes Witthöft <matteswitti@gmail.com>
2022-02-28 21:54:24 +0100

    add reducer to clear registration state

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,7 +1,7 @@
 import { createReducer } from '@reduxjs/toolkit';
 import { success, error } from '@redux-requests/core';
 import tokenService from '../services/tokenService';
-import { login, refreshToken, logout, register } from '../actions/auth';
+import { login, refreshToken, logout, register, clearRegisterState } from '../actions/auth';
 
 export default createReducer(
     {
@@ -37,6 +37,12 @@ export default createReducer(
         },
         [error(register)]: (state, action) => {
             state.registration.error = action.payload.response.data.message;
+        },
+        [success(clearRegisterState)]: (state, action) => {
+            state.registration = {
+                error: null,
+                success: null,
+            };
         },
     }
 );
