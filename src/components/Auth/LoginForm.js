bfe60636a5226b41096cbe9d0e2f133cad6b652e
Mattes Witthöft <matteswitti@gmail.com>
2022-02-28 21:08:48 +0100

    fix responsive design in login dialog

--- src/components/Auth/LoginForm.js
+++ src/components/Auth/LoginForm.js
@@ -58,16 +58,18 @@ const LoginForm = ({ open, handleClose }) => {
             <DialogContent>
                 <Box
                     sx={{
-                        display: 'flex',
+                        display: { sm: 'flex' },
                         flexDirection: 'column',
                         alignItems: 'center',
                     }}>
-                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Anmelden
-                    </Typography>
+                    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                            <LockOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Anmelden
+                        </Typography>
+                    </Box>
                     <form onSubmit={loginRequestPending ? null : handleSubmit}>
                         <FormGroup sx={{ width: showFullScreen ? '100%' : '28em' }}>
                             <TextField
