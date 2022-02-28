e3f71acae2774095c2f8365f98c5a027713de8cc
Mattes Witthöft <matteswitti@gmail.com>
2022-02-28 01:03:30 +0100

    use page reload to clear the hole data after logout

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -25,6 +25,7 @@ export default createReducer(
         [success(logout)]: (state, action) => {
             state.isLoggedIn = false;
             tokenService.setAccessToken(null);
+            window.location.reload();
         },
     }
 );
