4e99f2a0abfa5690ed58d95e4a98de40b5aa8642
Mattes Witthöft <matteswitti@gmail.com>
2022-02-28 21:52:59 +0100

    addd reducer for registration

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,13 +1,17 @@
 import { createReducer } from '@reduxjs/toolkit';
 import { success, error } from '@redux-requests/core';
 import tokenService from '../services/tokenService';
-import { login, refreshToken, logout } from '../actions/auth';
+import { login, refreshToken, logout, register } from '../actions/auth';
 
 export default createReducer(
     {
         isLoggedIn: false,
         error: null,
         user: null,
+        registration: {
+            error: null,
+            success: null,
+        },
     },
     {
         [success(login)]: (state, action) => {
@@ -27,6 +31,12 @@ export default createReducer(
             state.isLoggedIn = false;
             tokenService.setAccessToken(null);
             window.location.reload();
+        },
+        [success(register)]: (state, action) => {
+            state.registration.success = true;
+        },
+        [error(register)]: (state, action) => {
+            state.registration.error = action.payload.response.data.message;
         },
     }
 );
