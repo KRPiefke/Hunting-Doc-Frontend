ea7f6daf360243d55806ba79e8b5ab802f702447
Mattes Witthöft <matteswitti@gmail.com>
2022-03-02 21:44:55 +0100

    add password validation

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,5 +1,11 @@
 import Joi from 'joi';
 
+const passwordPattern = new RegExp(
+    /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*!@$%^&(){}[\]:#;<>,.?/~_+\-=|])(?:[0-9a-zA-Z*!@$%^&(){}[\]:#;<>,?/~_+\-=|]){8,32}$/
+);
+const passwordLowerCasePattern = new RegExp(/^(?!.*[a-z]{1}).+$/);
+const passwordUpperCasePattern = new RegExp(/^(?!.*[A-Z]{1}).+$/);
+const passwordSpecialCharPattern = new RegExp(/^(?!.*[*!@$%^&(){}[\]:#;<>,.?/~_+\-=|]{1}).+$/);
 const usernamePattern = new RegExp(/^(?!.*[_.-]{2})[0-9a-zA-Z_.-]+$/);
 const usernameRepeatetCharsPattern = new RegExp(/^(?=.*[_.-]{2}).+$/);
 
@@ -49,3 +55,27 @@ export const emailValidation = email => {
     return error?.details[0].message;
 };
 
+export const passwordValidation = password => {
+    if (passwordLowerCasePattern.test(password)) {
+        return 'Das Passwort muss mindestens einen Kleinbuchstaben enthalten.';
+    }
+    if (passwordUpperCasePattern.test(password)) {
+        return 'Das Passwort muss mindestens einen Großbuchstaben enthalten.';
+    }
+    if (passwordSpecialCharPattern.test(password)) {
+        return 'Das Passwort muss mindestens ein Sonderzeichen enthalten.';
+    }
+    let { error } = Joi.string()
+        .min(8)
+        .max(32)
+        .required()
+        .pattern(passwordPattern)
+        .messages({
+            'string.min': 'Das Passwort muss mindestens 8 Zeichen lang sein.',
+            'string.max': 'Das Passwort darf höchstens 32 Zeichen lang sein.',
+            'any.required': 'Das Passwort ist ein Pflichtfeld.',
+            'string.pattern.base': 'Das Passwort ist ungültig.',
+        })
+        .validate(password);
+    return error?.details[0].message;
+};
